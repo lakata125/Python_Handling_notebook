435c8fd625892aeff22e35fe4b586bdfa731f127
lakata125 <k.perney@campusber.fr>
2025-02-10 22:31:50 +0100

    Working day 2

--- SEQUENCE12_Using_modules.py
+++ SEQUENCE12_Using_modules.py
@@ -8,43 +8,37 @@ import random
 num = random.randint(1, 2)
 
 print("Welcome to the Coin Flipping Game!")
-choice = input("Enter your side (heads = 1 or tails = 2): ")[0]
+choice = int(input("Enter your side (heads = 1 or tails = 2): ")[0])
 
-if num == 1 :
-    if choice == "1" or choice == "h":
-        print("Good Job! You won. The coin flipped head")
-    else:
-        print("Aw... You lost. The coin flipped tails")
+if choice == num:
+    print("Good Job! You won.")
 else:
-    if choice == "2" or choice == "t":
-        print("Good Job! You won. The coin flipped tails")
-    else:
-        print("Aw... You lost. The coin flipped heads")
-        
+    print("Aw... You lost.")
+   
 print("\nThanks for playing. Goodbye")
 
-#print("num: ", num)
-#print("choice: ", choice)
+
 
 """
 
 #Exo2
 
 """
+
 import random
 dice = random.randint(1, 6)
 choice = 0
 
 print("Welcome to the dice game")
-choice = int(input("Choose a number you want between 1 to 6 ? ")[0])
+choice = int(input("\nChoose a number you want between 1 to 6 ? ")[0])
 
 if choice == dice:
-    print("Congratulation, you won the game with the number:", choice)
+    print(f"\nCongratulation, you won the game with the number: {dice}")
 else:
-    print("Aw... You lost the dice game with the number:", choice)
-print("\nThanks to have played at this simple dice game")
+    print("\nAw... You lost the dice game, the dice number was:", dice)
 
 """
+
 
 #OS MODULE
 
@@ -53,34 +47,52 @@ import os
 def directories(name):
     os.system("clear")
     cwd = os.getcwd()
-    print("Current Working Directory:", cwd)
+    #print("Current Working Directory:", cwd)
     path = os.path.join(cwd, name)
 
     if not os.path.exists(path):
         os.mkdir(path)
-        print("Directory '%s' created" %path)
+        print("\nDirectory created:", path)
     else:
-        print("Directory '%s' already exist" %path)
+        print("\nDirectory already existing:", path)
 
     os.chdir(path)
-    print("Print Working Directory: ", os.getcwd())
+    #print("Print Working Directory: ", os.getcwd())
+    hyphen = '-'
     
     i = 0       
     while i != 2:
         i += 1
-        folder = name + str(i)
+        folder = name + hyphen + str(i)
         sub_dir = os.path.join(path ,folder)
         
         if not os.path.exists(sub_dir):
             os.mkdir(sub_dir)
-            print("sub_dir '%s' created: " %sub_dir)
+            print("\nDirectory created:", sub_dir)
         else:
-            print("sub_dir '%s' already exist" %sub_dir)
+            print("\nDirectory already existing:", sub_dir)
         
     os.chdir(cwd)
-    print("Current Working Directory: ", os.getcwd())
-    print("Directories in: '", path, "' :")
+    #print("Current Working Directory: ", os.getcwd())
+    print("\nDirectories in: '", path, "' :")
     print(os.listdir(path))
 
-
 directories("TOTO")
+
+# Exo2
+
+def remove_dir():
+    answer = str(input("\nWrite the absolute path directory to remove it:\n"))
+    if not os.path.exists(answer):
+        print("\nDirectory not existing:", answer)
+    else:
+        list_dir = os.listdir(answer)
+        for dir in list_dir:
+            sub_dir = os.path.join(answer, dir)
+            print("\nSub directory removed:", sub_dir)
+            os.rmdir(sub_dir)
+        print("\nParent directory removed:", answer)
+        os.rmdir(answer)
+        
+
+remove_dir()
